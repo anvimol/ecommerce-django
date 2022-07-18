d4d9c93743c0b100470d3dce85a0d2c6743363d1
anvimol <anvimol@gmail.com>
2022-07-18 10:24:39 +0200

    Fin variations en carrito

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from store.models import Product
+from store.models import Product, Variation
 
 
 class ProductAdmin(admin.ModelAdmin):
@@ -7,5 +7,12 @@ class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('product_name',)}
 
 
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category', 'variation_value', 'is_active')
+    list_editable = ('is_active',)
+    list_filter = ('product', 'variation_category', 'variation_value', 'is_active')
+
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Variation, VariationAdmin)
 
